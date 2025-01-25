39c031682524289285b601a2680363fb7c7134f1
lyduybach2612 <lyduybach900@gmail.com>
2025-01-25 11:16:25 +0700

    add swagger denpendency

---
+++ src/main/java/com/bach/notes/dtos/responses/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.bach.notes.dtos.responses;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse <T>{
+
+    int status;
+    String message;
+    T data;
+
+}
